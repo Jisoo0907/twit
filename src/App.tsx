ef04d85306ba476e54833a0fcd231747c48b375d
jisoolee <szjslee@gmail.com>
2024-12-20 00:41:21 +0900

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // 인증된 사용자에게만 보여지게끔
+    children: [
+      {
+        path: "", // 비워두면 부모 경로와 동일(여기서는 "/")
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
