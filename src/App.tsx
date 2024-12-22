91dad66cc4b43e53d925ad0ddcd59c383ebe6ed3
jisoolee <szjslee@gmail.com>
2024-12-22 21:59:56 +0900

    feat: Firebase 인증 상태 확인, 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import reset from "styled-reset";
 import { createGlobalStyle } from "styled-components";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const router = createBrowserRouter([
   {
@@ -50,6 +51,7 @@ function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
     // wait for firebase
+    await auth.authStateReady(); // firebase 인증 상태 초기화/준비 완료될 때까지 대기
     setLoading(false);
   };
 
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAz3PlzVNWaE_g3Q6uMa_hOfy4pJJUto4A",
@@ -6,8 +7,10 @@ const firebaseConfig = {
   projectId: "twit-22352",
   storageBucket: "twit-22352.firebasestorage.app",
   messagingSenderId: "38041092",
-  appId: "1:38041092:web:0a9d93c22b46bfa1486751"
+  appId: "1:38041092:web:0a9d93c22b46bfa1486751",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); // config 옵션을 통한 app 생성
+
+export const auth = getAuth(app); // app에 대한 인증 서비스 사용
