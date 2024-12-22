4085df68d741b0f64ef7caf71b10e5b7a158411b
jisoolee <szjslee@gmail.com>
2024-12-22 18:50:36 +0900

    feat: initialize firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAz3PlzVNWaE_g3Q6uMa_hOfy4pJJUto4A",
+  authDomain: "twit-22352.firebaseapp.com",
+  projectId: "twit-22352",
+  storageBucket: "twit-22352.firebasestorage.app",
+  messagingSenderId: "38041092",
+  appId: "1:38041092:web:0a9d93c22b46bfa1486751"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
