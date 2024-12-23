0d853be86a03203a9c6aa51283c53af69ce25afa
jisoolee <szjslee@gmail.com>
2024-12-23 11:11:28 +0900

    feat: create account feature using firebase

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -41,6 +44,7 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -58,9 +62,20 @@ export default function CreateAccount() {
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+
+      await updateProfile(credentials.user, { displayName: name });
+      navigate("/");
     } catch (e) {
       // setError
     } finally {
@@ -74,7 +89,7 @@ export default function CreateAccount() {
 
   return (
     <Wrapper>
-      <Title>Log into Twitter</Title>
+      <Title>Join Twitter</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
