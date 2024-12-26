155f12a2940427751c363af505a7b894db6c1e53
jisoolee <szjslee@gmail.com>
2024-12-27 00:00:14 +0900

    feat: log in

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,47 +1,19 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import styled from "styled-components";
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import {
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-components";
 
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-const Error = styled.span`
-  font-weight: 600;
-  color: red;
-`;
+/* const errors = {
+  "auth/email-already-in-use": "That email already exists.",
+}; */
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -63,6 +35,7 @@ export default function CreateAccount() {
     }
   };
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setError(""); // 다시 제출 시 에러 메시지 초기화
     e.preventDefault();
     if (isLoading || name === "" || email === "" || password === "") return;
     try {
@@ -77,7 +50,9 @@ export default function CreateAccount() {
       await updateProfile(credentials.user, { displayName: name });
       navigate("/");
     } catch (e) {
-      // setError
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
     } finally {
       setLoading(false);
     }
@@ -121,6 +96,10 @@ export default function CreateAccount() {
         />
       </Form>
       {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account?
+        <Link to="/login"> Log in &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
